374b4569d981e6c183a6516e3e8ba74c80cada01
Arvind <arvindk@mindfiresolutions.com>
2017-04-04 18:23:41 +0530

    add custom dialog

---
+++ CustomAlertBox/app/src/main/java/com/example/arvindrathour/customalertbox/Dfragment.java
@@ -0,0 +1,84 @@
+package com.example.arvindrathour.customalertbox;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Dfragment extends DialogFragment{
+
+    String mBrandName;
+    boolean download;
+    boolean activate;
+    boolean contact;
+
+    public Dfragment() {
+        // Required empty public constructor
+    }
+
+    public void setBrandsDetail(String brandname,boolean optionOne,boolean optionTwo,boolean optionThree) {
+        // Required empty public constructor
+        mBrandName=brandname;
+        download =optionOne;
+        activate =optionTwo;
+        contact =optionThree;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView=inflater.inflate(R.layout.fragment_dfragment, container, false);
+
+         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+         setCancelable(false);
+
+//        setStyle(android.support.v4.app.DialogFragment.STYLE_NORMAL, R.style.PlaceOrderPopUpTheme);
+
+        TextView downloadTextView=(TextView)  rootView.findViewById(R.id.download);
+        TextView activateTextView=(TextView)  rootView.findViewById(R.id.activate);
+        TextView contactTextView=(TextView)  rootView.findViewById(R.id.conmtact);
+
+        if(!download)
+            downloadTextView.setVisibility(View.GONE);
+
+        if(!activate)
+            activateTextView.setVisibility(View.GONE);
+
+        if(!contact)
+            contactTextView.setVisibility(View.GONE);
+
+
+        downloadTextView.setText(String.format(getString(R.string.download_the_s_app), mBrandName));
+        activateTextView.setText(String.format(getString(R.string.activate_your_s_merchant_details), mBrandName));
+        contactTextView.setText(String.format(getString(R.string.contact_s_to_sign_up_as_a_merchant), mBrandName));
+
+
+
+        ImageButton cancel=(ImageButton) rootView.findViewById(R.id.image_button_cancel);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().cancel();
+            }
+        });
+        return rootView;
+    }
+
+}
---
+++ CustomAlertBox/app/src/main/java/com/example/arvindrathour/customalertbox/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.arvindrathour.customalertbox;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//    FragmentManager fm = getSupportFragmentManager();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View dialogOpen=(View) findViewById(R.id.button_dialog_open);
+
+        dialogOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dfragment dFragment = new Dfragment();
+                dFragment.setBrandsDetail("Coca-cola",false,true,true);
+                // Show DialogFragment
+                dFragment.show(getFragmentManager(), "Dialog Fragment");
+            }
+        });
+    }
+}
